317914b869d84a8e18b0d9924c896d655e932e42
Tyler Waugh <waughphoto@gmail.com>
2022-03-17 23:13:20 -0400

    style changes

--- matching-game.js
+++ matching-game.js
@@ -1,13 +1,14 @@
 
 // global variables
-let numberOfDogs = 5;
-let petDogs = 0; 
+let numberOfDogs = 5; 
+let petDogs = 0;
 let score = 0;
+let currentTime = 30
 const scoreDisplay = document.getElementById('score-display')
 const theLeftSide = document.getElementById("leftSide");
 const theRightSide = document.getElementById("rightSide");
 const dogArray = ['dog-1.png', 'dog-2.png', 'dog-3.png', 'dog-4.png'];
-
+const timeLeft = document.querySelector('#timer')
 // sounds
 const gameMusic = document.getElementById('game-music');
 const dogBark = document.getElementById('bark-sound');
@@ -25,6 +26,7 @@ function startGame() {
   generateDogs();
   startGameMusic();
   startGameButton.disabled = 'true';
+  let countDownTimerId = setInterval(countDown, 1000)
   
 }
 
@@ -80,11 +82,12 @@ function nextLevel() {
 }
 
 function gameOver () {
-  scoreDisplay.innerHTML = 0
   stopGameMusic();
+  // clearCountDown();
+  // score = 0;
   alert("Game Over!");
-  startGameButton.disabled = 'false';
   document.querySelector('#gameContainer').removeEventListener('click',gameOver); 
+  
   while (theLeftSide.firstChild) {
     theLeftSide.removeChild(theLeftSide.firstChild);
 }
@@ -93,6 +96,23 @@ while (theRightSide.firstChild) {
 }
   theLeftSide.lastChild.removeEventListener('click',nextLevel);
 }
+
+function countDown() {
+  currentTime--
+  timeLeft.textContent = currentTime
+ 
+  if (currentTime == 0) {
+    clearInterval(countDownTimerId)
+    alert('GAME OVER! Your final score is ' + score)
+  }
+ 
+ }
+
+ function clearCountDown() {
+  clearInterval(countDownTimerId)
+ }
+ 
+ 
 
 
 
